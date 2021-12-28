d8cdfee83d684ce8c2c7b0b8b87b613e5bdb8cd5
Block-Monkey <96780924+Block-Monkey@users.noreply.github.com>
2021-12-28 21:56:29 +0800

    Update and rename discordmonkey.user.js to blockermonkey.user.js

---
+++ blockermonkey.user.js
@@ -0,0 +1,1053 @@
+// ==UserScript==
+// @name         Blocker Monkey
+// @namespace    https://greasyfork.org/zh-TW/users/848949-kajipa-coder
+// @version      1.5
+// @description  An addon prevents children visiting Gaming Website.Made with JS.
+// @license      Blocker Monkey @ CopyRight 2022
+// @author       Jypa
+// @author       HahaLouisOMG 
+// @match        *://www.youtube.com*
+// @match        *://krunker.io/*
+// @match        *://web.roblox.com/home*
+// @match        *://roblox.com*
+// @match        *://discord.com*
+// @match        *://www.arkadium.com/*
+// @match        *://www.twitch.tv/*
+// @match        *://www.crazygames.com*
+// @match        *://classic.minecraft.net/*
+// @match        *://freeonlinegames.com/*
+// @match        *://www.yiv.com/*
+// @match        *://www.agame.com/*
+// @match        *://poki.com/*
+// @match        *://m.fog.com*
+// @match        *://www.bgames.com/*
+// @match        *://games.aarp.org/*
+// @match        *://slither.io/*
+// @match        *://viu.tv*
+// @match        *://www.instagram.com/*
+// @match        *://zh-hk.facebook.com/*
+// @match        *://faceit.com/*
+// @match        *://youtu.be/*
+// @grant        none
+// @icon         https://i.imgur.com/7sKkNi0.jpg
+// @support      https://discord.gg/F3wwA64TxX
+// @support      https://coding-club-forum.000webhostapp.com/
+// @run-at       document-start
+// ==/UserScript==
+//Loop Alert x253
+(function() {
+    'use strict';
+    alert("BLOCKING WEB LTD")
+})();
+  (function() {
+    'use strict';
+    alert("Blocking, Start Document")
+})();
+(function() {
+    'use strict';
+    alert("Looping x253...")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert(" ")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert(" ")
+})();
+(function() {
+    'use strict';
+    alert("  ")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert(" ")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("Looping x200 ")
+})();
+(function() {
+    'use strict';
+    alert("  ")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert(" ")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert(" ")
+})();
+(function() {
+    'use strict';
+    alert("  ")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert(" ")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
+(function() {
+    'use strict';
+    alert("")
+})();
